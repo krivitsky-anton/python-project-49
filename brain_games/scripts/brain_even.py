776dbf877d20f0ed9bd691dcf3cc0ecdf3e394b8
krivitskyanton <krivitsky.anton@gmail.com>
2024-04-28 01:10:40 +0200

    brain-even added

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,33 @@
+import prompt
+import random
+
+
+def get_random_number() -> int:
+    return random.randint(1, 100)
+
+
+def is_even(num: int):
+    if num % 2 == 0:
+        return 'yes'
+    return 'no'
+
+
+wins = 0
+
+print('Answer \"yes\" if the number is even, otherwise answer \"no\".')
+user_name = prompt.string('May I have your name? ')
+print(f'Hello, {user_name}!')
+while wins < 3:
+    number = get_random_number()
+    correct_answer = is_even(number)
+    user_answer = prompt.string(f'Question: {number} ')
+    if user_answer == correct_answer:
+        print('Correct!')
+        wins += 1
+    else:
+        print(f'\'{user_answer}\' is wrong answer ;(. Correct answer is \'{correct_answer}\'')
+        print(f'Let\'s try again, {user_name}!')
+        break
+
+if wins == 3:
+    print(f'Congratulations, {user_name}!')
